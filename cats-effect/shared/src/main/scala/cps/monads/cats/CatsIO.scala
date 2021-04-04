e6975d58d33176642ff77048e0c43ea1b6abef80
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-04-04 16:13:21 +0300

    removed code duplication

--- cats-effect/shared/src/main/scala/cps/monads/cats/CatsIO.scala
+++ cats-effect/shared/src/main/scala/cps/monads/cats/CatsIO.scala
@@ -13,37 +13,9 @@ import scala.concurrent._
 /**
  * CpsAsyncMonad for cats-effect.
  **/
-class CatsIOCpsAsyncMonad extends CpsAsyncMonad[IO]:
+class CatsIOCpsAsyncMonad extends CatsAsync[IO] with CpsAsyncMonad[IO]:
 
   type F[T] = IO[T]
-
-  def pure[A](a:A): IO[A] = IO.pure(a)
-
-  def map[A,B](fa:IO[A])(f: A=>B): IO[B] = 
-        fa.map(f)
-
-  def flatMap[A,B](fa:IO[A])(f: A=>IO[B]): IO[B] =
-        fa.flatMap(f)
-
-  def error[A](e: Throwable): IO[A] =
-        IO.raiseError[A](e)
-
-  def flatMapTry[A,B](fa: IO[A])(f: Try[A] => IO[B]): IO[B] =
-        fa.redeemWith( ex => f(Failure(ex)), a => f(Success(a)) )
-
-  override def mapTry[A,B](fa:IO[A])(f: Try[A]=>B): IO[B] =
-        fa.redeem(ex => f(Failure(ex)), a => f(Success(a)) )
-
-
-  def adoptCallbackStyle[A](source: (Try[A]=>Unit) => Unit): IO[A] =
-        def adoptIOCallback(ioCallback: Either[Throwable, A]=>Unit): Try[A]=>Unit = {
-           case Failure(ex) => ioCallback(Left(ex))
-           case Success(a) => ioCallback(Right(a))
-        }
-        IO.async_ {
-          ioCallback => source(adoptIOCallback(ioCallback))
-        }
-
 
 
 given catsIO: CpsAsyncMonad[IO] = CatsIOCpsAsyncMonad()

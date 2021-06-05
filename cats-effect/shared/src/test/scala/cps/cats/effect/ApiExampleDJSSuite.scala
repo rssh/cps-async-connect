7ebf9e99b71a26747e46753d8b3faa8c5418bc34
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-06-05 17:36:50 +0300

    added example from twitter conversation

---
+++ cats-effect/shared/src/test/scala/cps/cats/effect/ApiExampleDJSSuite.scala
@@ -0,0 +1,101 @@
+
+package cps.cats.effect
+
+import scala.language.implicitConversions
+import scala.util.*
+import scala.concurrent.duration.*
+
+import cats.*
+import cats.effect.*
+
+
+import cps.*
+import cps.automaticColoring.given
+
+import cps.monads.cats.{given,*}
+
+import munit.CatsEffectSuite
+
+
+object TestFuns {
+
+  def myFun(using RunContext): IO[Boolean] = async[IO] {
+
+    val results1 = talkToServer("request1", None)
+    IO.sleep(100.millis)
+ 
+    val results2 = talkToServer("request2", Some(results1.data)) 
+    if (results2.isOk) {
+       writeToFile(results2.data)
+       IO.println("done")
+       true
+    } else {
+       IO.println("abort abort abort")
+       false
+    }
+ 
+  }
+ 
+
+  case class Result(
+    isOk: Boolean,
+    data: String
+  )
+
+  case class RunContext(bingings: Map[String,Try[Result]])
+
+  def talkToServer(request:String, arg: Option[String])(using ctx: RunContext): IO[Result] = 
+    val key = s"talk-${request}-${arg.toString}"
+    handleKey(key) 
+
+  def writeToFile(data:String)(using ctx:RunContext): IO[Unit] =
+    val key = s"writeToFile-${data}"
+    handleKey(key).map(_ => ())
+
+  def handleKey(key: String)(using ctx: RunContext): IO[Result] =
+    ctx.bingings.get(key) match
+      case Some(tryValue) => 
+        tryValue match
+          case Success(r) => IO.delay(r)
+          case Failure(ex) => IO.raiseError(ex)
+      case None =>
+          IO.raiseError(RuntimeException(s"key $key is not found in run context"))
+
+}
+
+class ApiExampleDJSSuite extends CatsEffectSuite {
+
+  import TestFuns.*
+  
+  test("make sure than API text running ok when all is ok") {
+    given RunContext = RunContext(Map(
+      "talk-request1-None" -> Success(Result(true,"answer1")),
+      "talk-request2-Some(answer1)" -> Success(Result(true,"answer2")),
+      "writeToFile-answer2" -> Success(Result(true,"ok"))
+    )) 
+    TestFuns.myFun
+  }   
+
+  test("make sure than we return error when 1-st text failed") {
+    given RunContext = RunContext(Map(
+      "talk-request1-None" -> Failure(RuntimeException("x1")),
+      "talk-request2-Some(answer1)" -> Success(Result(true,"answer2")),
+      "writeToFile-answer2" -> Success(Result(true,"ok"))
+    )) 
+    interceptIO[RuntimeException](
+      TestFuns.myFun
+    )
+  }   
+
+  test("make sure than we return false when 2-st request is not ok") {
+    given RunContext = RunContext(Map(
+      "talk-request1-None" -> Success(Result(true,"answer1")),
+      "talk-request2-Some(answer1)" -> Success(Result(false,"answer2")),
+      "writeToFile-answer2" -> Success(Result(true,"ok"))
+    )) 
+    TestFuns.myFun.assertEquals(false)
+
+  }
+  
+
+}

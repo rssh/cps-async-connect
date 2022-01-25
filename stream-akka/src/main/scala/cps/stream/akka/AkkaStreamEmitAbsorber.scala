1eb485175beeec65799f50b8c4e7f44425540d4b
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-01-25 11:58:36 +0200

    ported stream to context-aware monads

--- stream-akka/src/main/scala/cps/stream/akka/AkkaStreamEmitAbsorber.scala
+++ stream-akka/src/main/scala/cps/stream/akka/AkkaStreamEmitAbsorber.scala
@@ -11,9 +11,12 @@ import cps.monads.{*,given}
 import cps.stream.{*,given}
 
 
-given AkkaStreamEmitAbsorber[T](using ExecutionContext):  BaseUnfoldCpsAsyncEmitAbsorber[Source[T,NotUsed],Future, FutureAsyncMonadAPI, T] with 
+given AkkaStreamEmitAbsorber[T](using ExecutionContext, Materializer):  BaseUnfoldCpsAsyncEmitAbsorber[Source[T,NotUsed],Future, CpsMonadInstanceContext[Future], T] with 
 
   override type Element = T
+
+  def asSync(fs: Future[Source[T,NotUsed]]):Source[T,NotUsed] =
+    Source.futureSource(fs).preMaterialize()._2
 
   def unfold[S](s0:S)(f:S => Future[Option[(T,S)]]): Source[T, NotUsed] =
         Source.unfoldAsync[S,T](s0)((s) => f(s).map(_.map{ case (x,y) => (y,x) }) )
--- stream-fs2/shared/src/main/scala/cpsfs2/Fs2AsyncEmitter.scala
+++ stream-fs2/shared/src/main/scala/cpsfs2/Fs2AsyncEmitter.scala
@@ -4,9 +4,12 @@ import cps.*
 import cps.stream.*
 import scala.concurrent.*
 
-given fs2EmitAbsorber[F[_]:CpsConcurrentMonad,T](using ExecutionContext):  BaseUnfoldCpsAsyncEmitAbsorber[fs2.Stream[F,T],F,T] with 
+given fs2EmitAbsorber[F[_],C <: CpsMonadContext[F],T](using ExecutionContext, CpsConcurrentMonad.Aux[F,C]):  BaseUnfoldCpsAsyncEmitAbsorber[fs2.Stream[F,T],F,C,T] with 
 
   override type Element = T
+
+  def asSync(fs: F[fs2.Stream[F,T]]): fs2.Stream[F,T] =
+        fs2.Stream.force(fs)
 
   def unfold[S](s0:S)(f:S => F[Option[(T,S)]]): fs2.Stream[F,T] =
         fs2.Stream.unfoldEval[F,S,T](s0)(f)

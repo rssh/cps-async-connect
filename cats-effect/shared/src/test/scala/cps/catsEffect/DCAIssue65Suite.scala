6243752f6cb81081f002904433cadaaaa0941b0a
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2023-01-22 12:23:43 +0200

    added test for iterator witj cats-effect

---
+++ cats-effect/shared/src/test/scala/cps/catsEffect/DCAIssue65Suite.scala
@@ -0,0 +1,55 @@
+package cps.catsEffect
+
+import scala.concurrent.*
+import scala.concurrent.duration.*
+
+import cats._
+import cats.effect._
+import cps.{async, asyncStream, await}
+import cps.stream.AsyncList
+import cps.monads.given
+import cps.monads.catsEffect.{*,given}
+
+
+import munit.*
+
+
+class DCAIssue65Suite extends FunSuite {
+
+  import cats.effect.unsafe.implicits.global
+
+  val N = 100_000
+
+  override val munitTimeout = Duration(300, "s")
+
+  given cps.macros.flags.PrintCode.type = cps.macros.flags.PrintCode
+
+  def readingByIterator(ec: ExecutionContext)(implicit loc: munit.Location):Future[Long] = {
+    given ExecutionContext = ec
+    val stream: AsyncList[IO, Int] = asyncStream[AsyncList[IO, Int]] { out =>
+      out.emit(0)
+      for i <- 1 to N do out.emit(i)
+    }
+    val ite = stream.iterator
+    val compute: IO[Long] = async[IO] {
+      var n = await(ite.next)
+      var res: Long = 0
+      while (n.nonEmpty) {
+        res += n.get
+        n = await(ite.next)
+      }
+      res
+    }
+    compute.unsafeToFuture()
+  }
+  
+  test("dotty-cps-async:65:global:reading by iterator with global execution context") {
+    readingByIterator(ExecutionContext.global)
+  }
+
+  test("dotty-cps-async:65:global:reading by iterator with parasitic execution context") {
+    readingByIterator(ExecutionContext.parasitic)
+  }
+
+
+}
